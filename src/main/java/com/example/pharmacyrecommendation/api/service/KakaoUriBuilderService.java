2246e85d1c04bf0e53d2e33ca3aa8f773d35560d
taewon0 <ktw495@naver.com>
2025-01-16 14:44:24 +0900

    feat: api를 사용해 리턴값을 받는 서비스와 api 호출을 위한 uri 생성 서비스 작성

---
+++ src/main/java/com/example/pharmacyrecommendation/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,25 @@
+package com.example.pharmacyrecommendation.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI buildUriByAddressSearch(String address){
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        builder.queryParam("query", address);
+
+        URI uri = builder.build().encode().toUri();
+        log.info("[KakaoUriBuilderService][buildUriByAddressSearch] address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+
+}
