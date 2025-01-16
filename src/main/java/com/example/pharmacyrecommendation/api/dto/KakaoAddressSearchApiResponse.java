16d771912bbb295466e50009d78415679984e124
taewon0 <ktw495@naver.com>
2025-01-16 14:56:20 +0900

    feat: kakao api response에 @JsonProperty 추가
    두 dto 모두 원래 json이름과 매핑한다.

--- src/main/java/com/example/pharmacyrecommendation/api/dto/KakaoAddressSearchApiResponse.java
+++ src/main/java/com/example/pharmacyrecommendation/api/dto/KakaoAddressSearchApiResponse.java
@@ -1,5 +1,6 @@
 package com.example.pharmacyrecommendation.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,9 @@ import java.util.List;
 @NoArgsConstructor
 public class KakaoAddressSearchApiResponse {
 
+    @JsonProperty("meta")
     private MetaDto metaDto;
 
+    @JsonProperty("documents")
     private List<DocumentDto> documentDtoList;
 }
