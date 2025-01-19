34dbc762b52305f4fd8620e98a0a16cbc5bac566
taewon0 <ktw495@naver.com>
2025-01-19 16:01:43 +0900

    feat: DocumentDto에 @Builder 추가
    빌더 패턴으로 생성가능하게 하기위해 빌더 추가

--- src/main/java/com/example/pharmacyrecommendation/api/dto/DocumentDto.java
+++ src/main/java/com/example/pharmacyrecommendation/api/dto/DocumentDto.java
@@ -2,9 +2,11 @@ package com.example.pharmacyrecommendation.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
