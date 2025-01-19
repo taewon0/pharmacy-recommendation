5cae34f4948968b157e3af7e2fd19723c97d6dfa
taewon0 <ktw495@naver.com>
2025-01-19 14:47:44 +0900

    feat: DirectionService 작성
    0. 입력 주소가 유효하지 않으면 빈 리스트 반환
    1. 모든 약국 리스트를 가져온다.
    2-1. 약국 리스트를 돌며 약국 정보와 입력 주소 정보를 이용해 Direction 객체를 구성한다.
    2-2. 구면 코사인 법칙 혹은 허버사인 공식을 사용해 입력 주소와 약국 사이의 거리를 계산해 Direction의 distance 필드에 넣어준다.
    3. 입력 주소의 기준 반경 이내에 있는 Direction만 필터링한다.
    4. distance를 기준으로 오름차순 정렬한다.
    5. 약국 추천 최대 개수만큼 추려 반환한다.
    
    구면 코사인 법칙과 허버사인 공식은 거의 같은 값을 가지지만 매우 짧은 거리를 계산할 때 오차가 있을 수 있어서 더 정확한 값을 원하면 허버사인 공식을 사용하는 것이 좋다.

---
+++ src/main/java/com/example/pharmacyrecommendation/direction/service/DirectionService.java
@@ -0,0 +1,78 @@
+package com.example.pharmacyrecommendation.direction.service;
+
+import com.example.pharmacyrecommendation.api.dto.DocumentDto;
+import com.example.pharmacyrecommendation.direction.entity.Direction;
+import com.example.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
+import com.example.pharmacyrecommendation.pharmacy.service.PharmacySearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DirectionService {
+
+    private static final int MAX_SEARCH_COUNT = 3; // 약국 추천 최대 개수
+    private static final double RADIUS_CUS_KM = 10; // 고객 반경 범위 10km
+
+    private final PharmacySearchService pharmacySearchService;
+
+    public List<Direction> buildDirectionList(DocumentDto documentDto){
+
+        if (Objects.isNull(documentDto)) return Collections.emptyList();
+
+        return pharmacySearchService.searchPharmacyDtoList()
+                .stream().map(pharmacyDto -> Direction.builder()
+                        .inputAddress(documentDto.getAddressName())
+                        .inputX(documentDto.getX())
+                        .inputY(documentDto.getY())
+                        .targetAddress(pharmacyDto.pharmacyAddress())
+                        .targetPharmacyName(pharmacyDto.pharmacyName())
+                        .targetX(pharmacyDto.x())
+                        .targetY(pharmacyDto.y())
+                        .distance(calculateDistanceByHaversine(documentDto.getX(), documentDto.getY(), pharmacyDto.x(), pharmacyDto.y()))
+                        .build())
+                .filter(direction -> direction.getDistance() <= RADIUS_CUS_KM)
+                .sorted(Comparator.comparing(Direction::getDistance))
+                .limit(MAX_SEARCH_COUNT)
+                .toList();
+
+
+
+    }
+
+    // 구면 코사인 법칙
+    private double calculateDistanceBySphericalLawOfCosine(double x1, double y1, double x2, double y2){
+        x1 = Math.toRadians(x1);
+        y1 = Math.toRadians(y1);
+        x2 = Math.toRadians(x2);
+        y2 = Math.toRadians(y2);
+
+        double earthRadius = 6371;
+
+        return earthRadius * Math.acos(Math.sin(y1) * Math.sin(y2) + Math.cos(y1) * Math.cos(y2) * Math.cos(x1 - x2));
+    }
+
+    // Haversine formula
+    private double calculateDistanceByHaversine(double x1, double y1, double x2, double y2){
+        double earthRadius = 6371; // 지구의 반지름 (킬로미터)
+
+        x1 = Math.toRadians(x1);
+        y1 = Math.toRadians(y1);
+        x2 = Math.toRadians(x2);
+        y2 = Math.toRadians(y2);
+
+        double SqSinDeltaY = Math.pow(Math.sin(Math.abs(y2 - y1) / 2), 2);
+        double SqSinDeltaX = Math.pow(Math.sin(Math.abs(x2 - x1) / 2), 2);
+
+        double sqrt = Math.sqrt(SqSinDeltaY + Math.cos(y1) * Math.cos(y2) * SqSinDeltaX);
+
+        return 2 * earthRadius * Math.asin(sqrt);
+    }
+}
