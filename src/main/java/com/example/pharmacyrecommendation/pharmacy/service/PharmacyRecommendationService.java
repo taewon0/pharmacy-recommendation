92ee89f71f3d7df75c172fd3c726e5c32f076b1c
taewon0 <ktw495@naver.com>
2025-01-19 17:12:20 +0900

    feat: PharmacyRecommendationService 추가
    최종적으로 입력받은 주소를 kakao api를 통해 좌표로 변환 후, 해당 좌표와 db에 있는 약국 리스트와의 거리를 계산해 가까운 약국 3개를 추출해 리턴 받아 direction db에 저장한다.

---
+++ src/main/java/com/example/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,44 @@
+package com.example.pharmacyrecommendation.pharmacy.service;
+
+import com.example.pharmacyrecommendation.api.dto.DocumentDto;
+import com.example.pharmacyrecommendation.api.dto.KakaoAddressSearchApiResponse;
+import com.example.pharmacyrecommendation.api.service.KakaoAddressSearchService;
+import com.example.pharmacyrecommendation.direction.entity.Direction;
+import com.example.pharmacyrecommendation.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    public void recommendPharmacyList(String address){
+
+        KakaoAddressSearchApiResponse response = kakaoAddressSearchService.requestAddressSearch(address);
+
+        if (Objects.isNull(response)) {
+            log.error("[PharmacyRecommendationService][recommendPharmacyList] Null returned. address: {}", address);
+        }
+
+        if (CollectionUtils.isEmpty(response.getDocumentDtoList())){
+            log.error("[PharmacyRecommendationService][recommendPharmacyList] Empty list returned. address: {}", address);
+        }
+
+        DocumentDto documentDto = response.getDocumentDtoList().get(0);
+
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        directionService.saveAll(directionList);
+
+    }
+
+}
