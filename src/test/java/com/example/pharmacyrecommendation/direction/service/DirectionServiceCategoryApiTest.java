7166bc7d17d0706d306e66da7c0d983498dfee12
taewon0 <ktw495@naver.com>
2025-01-20 16:44:43 +0900

    test: category api가 적절한 리턴을 하는지 테스트
    api를 직접 호출하여 테스트. assertJ의 assertThat으로 적절한 리턴값인지 검증
    자바 junit 테스트를 사용하기 위해 yml의 active 프로필 변경

---
+++ src/test/java/com/example/pharmacyrecommendation/direction/service/DirectionServiceCategoryApiTest.java
@@ -0,0 +1,40 @@
+package com.example.pharmacyrecommendation.direction.service;
+
+import com.example.pharmacyrecommendation.api.dto.DocumentDto;
+import com.example.pharmacyrecommendation.direction.entity.Direction;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class DirectionServiceCategoryApiTest {
+
+    @Autowired
+    private DirectionService directionService;
+
+    @Test
+    void buildDirectionListByCategoryApi_isReturnedCorrectly() {
+        //given
+        DocumentDto documentDto = DocumentDto.builder()
+                .x(126.874890556801)
+                .y(37.5533141837481)
+                .addressName("서울 강서구 양천로75길 57")
+                .build();
+        //when
+        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+
+        //then
+
+        // 리턴 결과 3개인지 확인
+        assertThat(directionList.size()).isEqualTo(3);
+        // 리턴 결과 정렬 확인
+        assertThat(directionList.get(0).getDistance()).isLessThan(directionList.get(1).getDistance());
+        // 리턴 결과 반경 2km 이내인지 확인
+        assertThat(directionList).allMatch(direction -> direction.getDistance() <= 2);
+
+    }
+}
