eec96514e6250395082f014d3851631238fba970
taewon0 <ktw495@naver.com>
2025-01-21 15:36:16 +0900

    test: MockMvc를 이용해 FormController 유닛 테스트
    standaloneSetup: 유닛 테스트
    
    컨트롤러에 사용되는 서비스는 모킹하여 주입한다.
    서비스가 반환하게 할 약국 리스트를 임의로 만들어둔다.
    
    MockMvc의 perform으로 요청을 보내고 andExpect로 status, methodName, handlertype, view name, model 존재 여부, model 값을 검증할 수 있다.
    andDo(log())로 로그를 확인할 수 있지만 나오지 않아 print() 사용.
    
    모킹 서비스의 행위를 then절에서 정의하며 1번 호출되었는지 확인 및 argument에 대한 검증도 진행.

---
+++ src/test/groovy/com/example/pharmacyrecommendation/direction/controller/FormControllerTest.groovy
@@ -0,0 +1,65 @@
+package com.example.pharmacyrecommendation.direction.controller
+
+import com.example.pharmacyrecommendation.direction.dto.OutputDto
+import com.example.pharmacyrecommendation.pharmacy.service.PharmacyRecommendationService
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+class FormControllerTest extends Specification {
+
+    private MockMvc mockMvc;
+    private PharmacyRecommendationService pharmacyRecommendationService = Mock()
+    private List<OutputDto> outputDtoList;
+
+    def setup(){
+        mockMvc = standaloneSetup(new FormController(pharmacyRecommendationService))
+        .build()
+
+        outputDtoList = new ArrayList<>()
+        outputDtoList.addAll(
+                OutputDto.of("pharmacy1"),
+                OutputDto.of("pharmacy2")
+        )
+    }
+
+    def "GET /"(){
+        expect:
+        // FormController의 "/" URI를 GET 방식으로 호출
+        mockMvc.perform(get("/"))
+            .andExpect(handler().handlerType(FormController.class))
+            .andExpect(handler().methodName("main"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("main"))
+            .andDo(print())
+    }
+
+    def "POST /search"(){
+        given:
+        String inputAddress = "서울 강서구 공항대로 290"
+
+        when:
+        def resultActions = mockMvc.perform(post("/search")
+                .param("address", inputAddress))
+
+        then:
+        1 * pharmacyRecommendationService.recommendPharmacyList(argument -> {
+            assert argument == inputAddress
+        }) >> outputDtoList
+
+        resultActions
+            .andExpect(status().isOk())
+            .andExpect(view().name("output"))
+            .andExpect(model().attributeExists("outputFormList"))
+            .andExpect(model().attribute("outputFormList", outputDtoList))
+            .andDo(print())
+
+        // FormController의 "/search"를 POST 방식으로 호출
+
+    }
+
+}
